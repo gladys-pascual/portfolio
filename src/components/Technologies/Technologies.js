fcc21c067272a13862abd593b2e27c31db3959db
gladys-pascual <gladyskate@gmail.com>
2022-04-26 23:52:04 +0100

    Add tooltip to technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,10 +1,14 @@
-import React from "react";
-import "./Technologies.scss";
+import React from 'react';
+import './Technologies.scss';
+import ReactTooltip from 'react-tooltip';
 
 const Technologies = ({ imgUrl, title }) => {
   return (
     <div className="technologies-wrapper">
-      <img src={imgUrl} alt={title} />
+      <img src={imgUrl} alt={title} data-tip data-for={imgUrl} />
+      <ReactTooltip id={imgUrl} type="light">
+        <span>{title}</span>
+      </ReactTooltip>
     </div>
   );
 };
